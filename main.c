1c9b2b5b235cd4d682e54ad0a8ad86f1fee8978d
DJGCrusader <dgonz@mit.edu>
2012-07-24 14:45:44 -0400

    Strange error, C not leaving functions or for loops. Very odd, going to try another version of gcc

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 //#include <math.h>
 
 void doItFast(){
-	int i;
-	for (i=0;i<10;i++){
 		PORTD.OUTSET = _BV(4);
         PORTD.OUTCLR = _BV(5);
         _delay_ms(100);
@@ -14,12 +12,10 @@ void doItFast(){
         PORTD.OUTCLR = _BV(4);
         PORTD.OUTSET = _BV(5);
         _delay_ms(100);
-	}
+	
 }
 
 void doItSlow(){
-	int i;
-	for (i=0;i<10;i++){
 		PORTD.OUTSET = _BV(4);
         PORTD.OUTCLR = _BV(5);
         _delay_ms(1000);
@@ -27,16 +23,17 @@ void doItSlow(){
         PORTD.OUTCLR = _BV(4);
         PORTD.OUTSET = _BV(5);
         _delay_ms(1000);
-	}
 }
+
+
 
 int main (void)
 {
-		
         PORTD.DIRSET = _BV(4);  
         PORTD.DIRSET = _BV(5); 
-//		doItFast();
+		doItFast();
 		doItSlow();
 		while(1){
+			doItFast();
 		}
 }
