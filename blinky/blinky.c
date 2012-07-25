360d20f8ac88bf3c681a9295128770c4ab2f336d
DJGCrusader <dgonz@mit.edu>
2012-07-25 06:32:55 -0400

    set blinky to 32 MHz

--- blinky/blinky.c
+++ blinky/blinky.c
@@ -1,51 +1,76 @@
-#define F_CPU 2000000UL /*2 MHz Internal Oscillator*/
+#define F_CPU 	32000000UL /*32 MHz Internal Oscillator*/
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "requiredFiles/avr_compiler.h"
+#include "requiredFiles/clksys_driver.h"
+#include "requiredFiles/clksys_driver.c"
 //#include <math.h>
-void doItFast(void);
-void doItSlow(void);
-int main(void);
+
+void delay_ms(int ms);
+void blink(int val);
+void blinkFast(void);
+void blinkSlow(void);
+void setClock(void);
+void init(void);
 void mainLoop(void);
+int main(void);
 
-void doItFast(void){
-	int i;
-		for (i=0;i<10;i++){
-			
-		PORTD.OUTSET = _BV(4);
-        PORTD.OUTCLR = _BV(5);
-        _delay_ms(100);
+void delay_ms(int ms) 
+{ 
+    while (ms--) 
+        _delay_us(1000); 
+} 
 
-        PORTD.OUTCLR = _BV(4);
-        PORTD.OUTSET = _BV(5);
-        _delay_ms(100);
-        
-	}
+void init(void){
+		PORTD.DIRSET = _BV(4);  
+        PORTD.DIRSET = _BV(5);
 }
 
-void doItSlow(void){
+void blink(int myVal){
 		PORTD.OUTSET = _BV(4);
         PORTD.OUTCLR = _BV(5);
-        _delay_ms(1000);
+        delay_ms(myVal);
 
         PORTD.OUTCLR = _BV(4);
         PORTD.OUTSET = _BV(5);
-        _delay_ms(1000);
+        delay_ms(myVal);
+}
+
+void blinkSlow(void){
+	blink(2000);
+}
+
+void blinkFast(void){
+	int i;
+	for (i=0;i<10;i++){
+		
+		blink(100);
+	}
+	
 }
 
 void mainLoop(void){
 		while(1){
-			doItFast();
-			doItSlow();
+			blink(1000);
+			blinkSlow();
+			blinkFast();
 		}
+}
+
+void setClock(void){
+    // Configure Clock -- Based on Application Notes & Examples on AVRFreaks
+	CLKSYS_Enable( OSC_RC32MEN_bm );
+	do {} while ( CLKSYS_IsReady( OSC_RC32MRDY_bm ) == 0 );
+	CLKSYS_Main_ClockSource_Select( CLK_SCLKSEL_RC32M_gc );
 }
 
 
 
 int main (void)
-{
-        PORTD.DIRSET = _BV(4);  
-        PORTD.DIRSET = _BV(5); 
+{	
+		setClock();
+		init();
         mainLoop();
         return 0;
 }
